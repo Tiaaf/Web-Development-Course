8d63705ab5b554c31e521214c644f4738376593e
Tiaaf <adam.faci@telecomnancy.net>
2017-04-16 22:45:03 +0200

    changed some things to redirect urls and validate Unit 5. Unit 5 done !

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from handlers.fizzbuzz import *
 from handlers.signup import *
 from handlers.blog import *
 from handlers.visits import *
-from handlers.json import *
+from handlers.blogJson import *
 
 app = webapp2.WSGIApplication([('/',MainPage),
                                ('/birthdate',Birthdate),
@@ -16,13 +16,15 @@ app = webapp2.WSGIApplication([('/',MainPage),
                                ('/ascii',ASCII),
                                ('/shopping',Shopping),
                                ('/fizzbuzz',Fizzbuzz),
-                               ('/signup',SignUp),
-                               ('/login',Login),
-                               ('/logout',Logout),
-                               ('/signup/welcome',Welcome),
+                               ('/blog/signup',SignUp),
+                               ('/blog/login',Login),
+                               ('/blog/logout',Logout),
+                               ('/blog/welcome',Welcome),
                                ('/blog',Blog),
                                ('/blog/newpost',NewPost),
                                ('/blog/(\d+)',IdPost),
+                               ('/blog/.json',BlogJson),
+                               ('/blog/(\d+)(?:.json)',IdPostJson),
                                ('/visits',Visits)
                                 ],
                                debug=True)
